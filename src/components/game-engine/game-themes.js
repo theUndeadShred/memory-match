9230c201f71e5de3c67bf560483c6050072fead9
theUndeadShred <undeadShredz@gmail.com>
2024-03-25 16:32:31 -0600

    fix path src for assets

--- src/components/game-engine/game-themes.js
+++ src/components/game-engine/game-themes.js
@@ -1,32 +1,32 @@
 export const mario = [
   {
     id: 1,
-    name: "Peach",
-    img: "http://localhost:5173//public/nintendo/peach.png",
+    name: 'Peach',
+    img: '/nintendo/peach.png',
   },
   {
     id: 2,
-    name: "Mario",
-    img: "http://localhost:5173//public/nintendo/mario.webp",
+    name: 'Mario',
+    img: '/nintendo/mario.webp',
   },
   {
     id: 3,
-    name: "Yoshi",
-    img: "http://localhost:5173//public/nintendo/pink-yoshi.webp",
+    name: 'Yoshi',
+    img: '/nintendo/pink-yoshi.webp',
   },
   {
     id: 4,
-    name: "Toad",
-    img: "http://localhost:5173//public/nintendo/toad.webp",
+    name: 'Toad',
+    img: '/nintendo/toad.webp',
   },
   {
     id: 5,
-    name: "Luigi",
-    img: "http://localhost:5173//public/nintendo/lugii.webp",
+    name: 'Luigi',
+    img: '/nintendo/lugii.webp',
   },
   {
     id: 6,
-    name: "Wiggler",
-    img: "http://localhost:5173//public/nintendo/wiggler.webp",
+    name: 'Wiggler',
+    img: '/nintendo/wiggler.webp',
   },
 ];
