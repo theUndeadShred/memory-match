e0d4634e28709945626eb8c1c938dd9425c183f7
theUndeadShred <undeadShredz@gmail.com>
2024-03-25 16:15:53 -0600

    Update server config for imgs

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '^/public/.*': {
+        target: 'http://localhost:5173',
+        changeOrigin: true,
+      },
+    },
+  },
   plugins: [react()],
-})
+});
